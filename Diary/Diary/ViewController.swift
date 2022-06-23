088adf5b458169821f3472ef115adc391b833e3d
최지혁 <zhzh1x4@gmail.com>
2022-06-23 23:49:20 +0900

    update

--- Diary/Diary/ViewController.swift
+++ Diary/Diary/ViewController.swift
@@ -11,47 +11,110 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    private var diaryList = [Diary]()
+    private var diaryList = [Diary]() {
+        didSet {
+            self.saveDiaryList()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureCollectionView()
+        self.loadDiaryList()
     }
     
+    //콜렉션 뷰
     private func configureCollectionView() {
         self.collectionView.collectionViewLayout = UICollectionViewLayout()
         self.collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
+        loadDiaryList()
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //segue로 이동하는 뷰컨트롤러가 무엇인지 판단 
         if let writeDiaryViewController = segue.destination as? WriteDiaryViewController {
             writeDiaryViewController.delegate = self
         }
     }
     
+    private func saveDiaryList() {
+        let date = self.diaryList.map {
+            [
+                "title": $0.title,
+                "contents": $0.contents,
+                "date": $0.date,
+                "isStar": $0.isStar
+            ]
+        }
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(date, forKey: "diaryList")
+    }
+    
+    private func loadDiaryList() {
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.object(forKey: "diaryList") as? [[String: Any]] else { return }
+        self.diaryList = data.compactMap {
+            guard let title = $0["title"] as? String else { return nil }
+            guard let contents = $0["contents"] as? String else { return nil }
+            guard let date = $0["date"] as? Date else { return nil }
+            guard let isStar = $0["isStar"] as? Bool else { return nil }
+            return Diary(title: title, contents: contents, date: date, isStar: isStar)
+            
+        }
+    }
+    
     private func dateToString(date: Date) -> String {
         let formatter = DateFormatter()
-        
+        formatter.dateFormat = "yy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter.string(from: date)
     }
 
 }
 
+//콜렉션 뷰로 보여지는 데이터를 관리하는 역할
 extension ViewController: UICollectionViewDataSource {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
+    //필수 메서드
+    //지정된 섹션에 표시할 셀의 표시할 개수를 반환
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.diaryList.count
     }
     
+    //필수 메서드
+    //콜렉션뷰에 지정된 위치에 표시할 셀을 요청하는 메서드
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //스토리보드에서 구성한 셀을 가져온다.
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DiaryCell", for: indexPath) as? DiaryCell else { return UICollectionViewCell() }
         let diary = self.diaryList[indexPath.row]
         cell.titleLabel.text = diary.title
-        cell.dateLabel.text =
+        cell.dateLabel.text = dateToString(date: diary.date)
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    //cell에 사이즈 설정 가능
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (UIScreen.main.bounds.width / 2) - 20, height: 200)
     }
 }
 
 extension ViewController: WriteDiaryViewDelegate {
     func didSelectRegister(diary: Diary) {
         self.diaryList.append(diary)
+        self.collectionView.reloadData()
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let viewController = storyboard?.instantiateViewController(identifier: "DiaryDetailViewController") as? DiaryDetailViewController else { return }
+        //let diary = diaryList[indexPath.row]
+        //viewController.diary = diary
+        //viewController.indexPath = indexPath
+//        viewController.delegate = self
+        navigationController?.pushViewController(viewController, animated: true)
     }
 }
